f6b5fa37f0e1bca62358bd0507563fcbb47def34
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-03-04 12:18:46 +0200

    implemented overall prob to have flipped inputs

--- flipfromjustflipped.py
+++ flipfromjustflipped.py
@@ -8,22 +8,25 @@ import networkclass as nc
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-K","--maxK",type=int,default=31)
+    parser.add_argument("-K","--maxK",type=int,default=16)
     parser.add_argument("-S","--steps",type=int,default=30)
     parser.add_argument("-o","--outbasename",type=str,default="out")
     args = parser.parse_args()
 
-
-
-    klist = np.arange(start = 3, stop = args.maxK + 1e-6, step = 2,dtype = int)
-    steps = np.arange(args.steps,dtype=int)
+    klist = np.arange(start = 3, stop = args.maxK + 1e-6, step = 2, dtype = int)
+    steps = np.arange(args.steps, dtype = int)
     
     for k in klist:
         print(k)
-        tmpflip = np.zeros(args.steps)
+        isflipped = np.ones(args.steps + 1)
+        flipprob = np.zeros(args.steps)
         for s in steps:
-            tmpflip[s] = np.sum(nc.ProbGF(s,k,(k+1)/2)[:(k+1)/2])
-        np.savetxt(args.outbasename + '_{:03d}'.format(k),np.array([steps,tmpflip]).T)
+            flipprob[s] = np.sum(nc.ProbGF(s, k, (k+1)/2)[:(k+1)/2])
+            isflipped[s+1:] *= (1. - flipprob[s])
+            isflipped[s]  *= flipprob[s]
+        
+        np.savetxt(args.outbasename + '_{:03d}'.format(k),np.array([steps,flipprob,isflipped[:s+1]]).T)
+
 
 if __name__ == "__main__":
     main()
