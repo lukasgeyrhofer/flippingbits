59b8ea1f1a37e8fc9ffea2735be067159fc9ce33
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-03-28 14:48:23 +0200

    combined functionality of 2 scripts

--- ProcessSimulationOutput.py
+++ ProcessSimulationOutput.py
@@ -11,7 +11,8 @@ def main():
     parser.add_argument("-i", "--infiles",          default = [],     type = str, nargs = "*")
     parser.add_argument("-o", "--outfileprefix",    default = "norm", type = str)
     parser.add_argument("-v", "--verbose",          default = False,  action = "store_true")
-    parser.add_argument("-O", "--OverwriteInPlace", default = False, action = "store_true")
+    parser.add_argument("-O", "--OverwriteInPlace", default = False,  action = "store_true")
+    parser.add_argument("-C", "--Cumulative",       default = False,  action = "store_true")
     args = parser.parse_args()
 
     for fn in args.infiles:
@@ -23,29 +24,37 @@ def main():
         n                  = data[:,0]
         Pxfn               = data[:,1]
         Psfn               = data[:,2]
-
         Pxfgn              = np.zeros(len(n))
         Pxfgn[data[:,6]>0] = data[:,5][data[:,6]>0]/data[:,6][data[:,6]>0]
         Psfgn              = np.zeros(len(n))
         Psfgn[data[:,4]>0] = data[:,3][data[:,4]>0]/data[:,4][data[:,4]>0]
+        output             = np.array([n,Pxfn,Psfn,Pxfgn,Psfgn], dtype = np.float).T
 
         columnheaders      = ['n', 'P[xf,n]', 'P[sf,n]', 'P[xf|n]', 'P[sf|n]']
-        output             = np.array([n,Pxfn,Psfn,Pxfgn,Psfgn], dtype = np.float).T
         
         if np.shape(data)[1] > 7:
             Pxfngsf        = data[:,7]
             Pxfngsf       /= np.sum(Pxfngsf)
             Psfngxf        = data[:,8]
             Psfngxf       /= np.sum(Psfngxf)
+            output         = np.concatenate([output,np.array([Pxfngsf,Psfngxf], dtype = np.float).T], axis = 1)
 
             columnheaders.append('P[xf,n|sf]')
             columnheaders.append('P[sf,n|xf]')
             
-            output         = np.concatenate([output,np.array([Pxfngsf,Psfngxf], dtype = np.float).T], axis = 1)
+        
+        if args.Cumulative:
+            Cxfn           = np.array([np.sum(Pxf[:m]) for m in range(len(n))])
+            Csfn           = np.array([np.sum(Psf[:m]) for m in range(len(n))])
+            output         = np.concatenate([output, np.array([Cxfn,Csfn], dtype = np.float).T], axis = 1)
+            
+            columnheaders.append('P[xf,<n]')
+            columnheaders.append('P[sf,<n]')
             
             
         if args.OverwriteInPlace:   outfilename = fn
         else:                       outfilename = args.outfileprefix + '_' + os.path.basename(fn)
+        
         np.savetxt(outfilename, output, header = ' '.join(['{:>14s}'.format(h + '  ') for h in columnheaders]), fmt = '%14.6e')
 
 
