cc789763103bacf26ea2b1641a530852750d3a9d
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2019-03-28 18:05:13 +0200

    fft up to arbitrary order

--- CheckDistributions.py
+++ CheckDistributions.py
@@ -11,17 +11,15 @@ from os.path import basename
 def revert(x):
     return x[::-1]
 
-
-
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--infiles",       default = [],           type = str, nargs = "*",)
-    parser.add_argument("-o", "--outfileprefix", default = "CheckDistr", type = str)
-    parser.add_argument("-F", "--fftoutfiles",   default = "CDfft",      type = str)
-    parser.add_argument("-m", "--maxtime",       default = None,         type = int)
-    parser.add_argument("-v", "--verbose",       default = False,        action = "store_true")
-    parser.add_argument("-f", "--fourthorder",   default = False,        action = "store_true")
+    parser.add_argument("-i", "--infiles",           default = [],           type = str, nargs = "*",)
+    parser.add_argument("-o", "--outfileprefix",     default = "CheckDistr", type = str)
+    parser.add_argument("-F", "--fftoutfiles",       default = "CDfft",      type = str)
+    parser.add_argument("-m", "--maxtime",           default = None,         type = int)
+    parser.add_argument("-v", "--verbose",           default = False,        action = "store_true")
+    parser.add_argument("-C", "--DirectConvolution", default = False,        action = "store_true")
+    parser.add_argument("-M", "--maxorderFFT",       default = 4,            type = int)
     args = parser.parse_args()
 
     for fn in args.infiles:
@@ -41,51 +39,72 @@ def main():
         Pxfngsf = data[:maxtime,5] # P[xf,n|sf]
         Psfngxf = data[:maxtime,6] # P[sf,n|xf]
 
-        PxfnCOMP0 = np.array([np.dot(Psfngxf[:m],revert(Pxfngsf[:m])) for m in range(len(n))])
+        PxfnCOMP0 = np.array([np.dot(Psfngxf[:m],revert(Pxfngsf[:m])) for m in range(maxtime)])
+        
         PxfnCOMP2 = np.zeros(maxtime)
-        for m in range(maxtime):
-            for k in range(maxtime-m):
-                for l in range(maxtime-m-k):
-                    PxfnCOMP2[m] += Psfngxf[m] * Psfn[k] * Psfn[l] * Pxfngsf[maxtime-m-k-l-1]
+        if args.DirectConvolution:
+            for m in range(maxtime):
+                for k in range(maxtime-m):
+                    for l in range(maxtime-m-k):
+                        for i in range(maxtime-m-k-l):
+                            PxfnCOMP2[m] += Psfngxf[k] * Psfn[l] * Psfn[i] * Pxfngsf[m-k-l-i-1]
         
         PxfnCOMP4 = np.copy(PxfnCOMP2)
-        if args.fourthorder:
+        if args.DirectConvolution:
             for m in range(maxtime):
                 for k in range(maxtime-m):
                     for l in range(maxtime-m-k):
                         for i in range(maxtime-m-k-l):
                             for j in range(maxtime-m-k-l-i):
-                                PxfnCOMP4[m] += Psfngxf[m] * Psfn[k] * Psfn[l] * Psfn[i] * Psfn[j] * Pxfngsf[maxtime-m-k-l-i-j-1]
+                                for r in range(maxtime-m-k-l-i-j):
+                                    PxfnCOMP4[m] += Psfngxf[k] * Psfn[l] * Psfn[i] * Psfn[j] * Psfn[r] * Pxfngsf[m-k-l-i-j-r-1]
         
-        Fxfz    = np.fft.fft(Pxfn)
-        Fsfz    = np.fft.fft(Psfn)
-        Fxfgz   = np.fft.fft(Pxfgn)
-        Fsfgz   = np.fft.fft(Psfgn)
-        Fxfzgsf = np.fft.fft(Pxfngsf)
-        Fsfzgxf = np.fft.fft(Psfngxf)
+        
+        PxfnAT    = np.array([np.dot(Pxfn[:m],revert(Pxfn[:m])) for m in range(maxtime)])
+        PsfnAT    = np.array([np.dot(Psfn[:m],revert(Psfn[:m])) for m in range(maxtime)])
+        
+        
+        # use FFT to compute same distributions
+        z         = np.real(np.fft.fftfreq(maxtime))
+        Fxfz      = np.fft.fft(Pxfn)
+        Fsfz      = np.fft.fft(Psfn)
+        Fxfgz     = np.fft.fft(Pxfgn)
+        Fsfgz     = np.fft.fft(Psfgn)
+        Fxfzgsf   = np.fft.fft(Pxfngsf)
+        Fsfzgxf   = np.fft.fft(Psfngxf)
         
         FxfzCOMP0 = Fxfzgsf * Fsfzgxf
-        FxfzCOMP2 = Fxfzgsf * Fsfzgxf * (1 + Fsfz**2)
-        FxfzCOMP4 = Fxfzgsf * Fsfzgxf * (1 + Fsfz**2 + Fsfz**4)
+
+        FxfzAT    = Fxfz**2
+        FsfzAT    = Fsfz**2
         
-        fPxfn0 = np.real(np.fft.ifft(FxfzCOMP0))
-        fPxfn2 = np.real(np.fft.ifft(FxfzCOMP2))
-        fPxfn4 = np.real(np.fft.ifft(FxfzCOMP4))
+        fPxfnAT   = np.fft.ifft(FxfzAT)
+        fPsfnAT   = np.fft.ifft(FsfzAT)
         
-        FxfzAT = Fxfz**2
-        FsfzAT = Fsfz**2
         
-        fPxfnAT = np.fft.ifft(FxfzAT)
-        fPsfnAT = np.fft.ifft(FsfzAT)
-        
-        outfilename = args.outfileprefix + '_' + basename(fn) 
-        np.savetxt(outfilename, np.array([n, Pxfn, Psfn, Pxfgn, Psfgn, Pxfngsf, Psfngxf, fPxfn0, fPxfn2, fPxfn4, PxfnCOMP0, PxfnCOMP2, PxfnCOMP4, fPxfnAT, fPsfnAT], dtype = np.float).T)
-        #                                 1  2     3     4      5      6        7        8       9       10      11         12         13         14       15
+        # generate output matrices
+        output    = np.array([n, Pxfn, Psfn, Pxfgn, Psfgn, Pxfngsf, Psfngxf, np.real(fPxfnAT), np.real(fPsfnAT), PxfnAT, PsfnAT, PxfnCOMP0, PxfnCOMP2, PxfnCOMP4], dtype = np.float).T
+        #                     1  2     3     4      5      6        7        8                 9                 10      11      12         13         14
+
+        fftoutput = np.array([z, np.real(Fxfz), np.imag(Fxfz), np.real(Fsfz), np.imag(Fsfz)]).T
+        #                     1  2              3              4              5
+
+        FxfzCOMP  = dict()
+        fPxfnCOMP = dict()
+        for order in np.arange(start = 0, stop = args.maxorderFFT + 1e-2, step = 2, dtype = np.int):
+            FxfzCOMP[order]  = FxfzCOMP0 * np.sum([np.power(Fsfz,2*a) for a in range(order/2)])
+            fPxfnCOMP[order] = np.real(np.fft.ifft(FxfzCOMP[order]))
+            
+            output           = np.concatenate([output,    np.array( [np.real(fPxfnCOMP[order])] ).T], axis = 1)
+            fftoutput        = np.concatenate([fftoutput, np.array( [np.real(FxfzCOMP[order]), np.imag(FxfzCOMP[order])] ).T], axis = 1)
+
+
+        # write output to files
+        outfilename = args.outfileprefix + '_' + basename(fn)
+        np.savetxt(outfilename, output, fmt = '%14.6e')
         
         fftoutfile = args.fftoutfiles + '_' + basename(fn)
-        z = np.real(np.fft.fftfreq(maxtime))
-        np.savetxt(fftoutfile,np.array([z, np.real(Fxfz), np.imag(Fxfz), np.real(Fsfz), np.imag(Fsfz), np.real(FxfzCOMP0), np.imag(FxfzCOMP0), np.real(FxfzCOMP2), np.imag(FxfzCOMP2), np.real(FxfzCOMP4), np.imag(FxfzCOMP4)]).T)
-        #                               1  2              3              4              5              6                   7                   8                   9                   10                  11
+        np.savetxt(fftoutfile,fftoutput, fmt = '%14.6e')
         
                                           
 if __name__ == "__main__":                   
